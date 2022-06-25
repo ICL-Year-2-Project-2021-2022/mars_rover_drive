8569ea8c8b9fd26ea97b10de8968e662bd3f7081
TimothyJNewman <84350177+TimothyJNewman@users.noreply.github.com>
2022-06-25 11:59:19 +0100

    Working integration tuning code

--- rover_drive_optical/lib/Motor_Drive/Motor_Drive.h
+++ rover_drive_optical/lib/Motor_Drive/Motor_Drive.h
@@ -2,13 +2,13 @@
 // motor defns
 // motor 1 settings
 #define CHA 0
-#define ENA 14 // D10 // this pin must be PWM enabled pin if Arduino board is used
-#define IN1 15 // D12
-#define IN2 2 // D13
+#define ENA 14 // D10 PWMA // this pin must be PWM enabled pin if Arduino board is used
+#define IN1 15 // D12 A11
+#define IN2 2 // D13 A12
 // motor 2 settings
-#define IN3 21 // D3
-#define IN4 22 // D4
-#define ENB 4  // D11 this pin must be PWM enabled pin if Arduino board is used
+#define IN3 21 // D3 B11
+#define IN4 22 // D4 B12
+#define ENB 4  // D11 PWMB this pin must be PWM enabled pin if Arduino board is used
 #define CHB 1
 const int CCW = 2; // do not change
 const int CW = 1;  // do not change
